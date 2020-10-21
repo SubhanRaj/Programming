8a5eb6905ca090b1e25681222ae59cb731fb08d8
Subhan Raj <shubhanraj2002@gmail.com>
2020-10-21 09:27:55 +0530

    Deployed C on Ubuntu 20.04 LTS

--- C/Basics/helloworld.c
+++ C/Basics/helloworld.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 
 int main(){
+    int tom;
+    // This is a single line comment
+    /* This is a multi line 
+    comment*/
 
     printf("Hello, World! I'm Subhan Raj, and started learnig C language today as on 19-Oct-2020");
     return 0;
-}
+} 
