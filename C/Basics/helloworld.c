d4de40b1f1aa915e3f28b6971a1d0f316c70dddc
Subhan Raj <shubhanraj2002@gmail.com>
2020-10-22 10:36:53 +0530

    Added Coments, new variables and few more lines

--- C/Basics/helloworld.c
+++ C/Basics/helloworld.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+/*
+This is my first c
+programm, which is awesome
+*/
 
 int main(){
     int tom;
@@ -6,6 +10,7 @@ int main(){
     /* This is a multi line 
     comment*/
 
-    printf("Hello, World! I'm Subhan Raj, and started learnig C language today as on 19-Oct-2020");
+    printf("Hello, World! I'm Subhan Raj, and started learnig C language");
+    printf(" today as on 19-Oct-2020");
     return 0;
 } 
