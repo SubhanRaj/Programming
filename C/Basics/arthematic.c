d96604612d40f9d401dfd67436879189cdeda746
Subhan Raj <shubhanraj2002@gmail.com>
2020-11-07 12:52:02 +0530

    Basic Arthematics

--- C/Basics/arthematic.c
+++ C/Basics/arthematic.c
@@ -1,21 +1,40 @@
 #include <stdio.h>
 //This programm performs simple arthematic operations
 
-int main() 
+int main()
 {
     int a = 10;
     int b = 30;
-    int e = 10000+1+6+3;
-    float c =12.50;
+    int e = 10000 + 1 + 6 + 3;
+    int z = 10000 - (1 + 6 + 3);
+    float c = 12.50;
 
-    printf ("The value of a+b is %d \n" ,a+b);
-    printf ("and \n");
+    printf("\n");
+
+    printf("All operations below are addition \n");
+    printf("\n");
+    printf("The value of a+b is %d \n", a + b);
+    printf("and \n");
     printf("The value of e is %d \n", e);
     printf("and \n");
-    printf ("The Value of a+c is %f \n", a+c);
+    printf("The Value of a+c is %f \n", a + c);
     printf("and \n");
     printf("The Value of a+b+c is %f \n", a + b + c);
-    printf ("The programm compiled sucessfully, You have done it. \n");
+    printf("\n");
+    printf("The programm compiled sucessfully, You have done it. \n");
+    printf("\n");
+
+    printf("All operations below are Substraction \n");
+    printf("\n");
+    printf("The value of b-a is %d \n", b - a);
+    printf("and \n");
+    printf("The value of z is %d \n", z);
+    printf("and \n");
+    printf("The Value of c-a is %f \n", c - a);
+    printf("and \n");
+    printf("The Value of a+b-c is %f \n", a + b - c);
+    printf("\n");
+    printf("The programm compiled sucessfully, You have done it. \n");
 
     return 0;
 }
--- C/Basics/variables.c
+++ C/Basics/variables.c
@@ -2,23 +2,21 @@
 /* This program contains Integers, Real Variables
 & Character variables*/
 
-
 int main()
 {
     int a = 4;
-    
-    // int b = 8.69; 
-    /*This is not recommended because 8.69 is a real variable, not an integer*/ 
+
+    // int b = 8.69;
+    /*This is not recommended because 8.69 is a real variable, not an integer*/
 
     float b = 7.5;
     char c = 'B';
 
-    printf ("The Value of a is %d \n" ,a ); 
+    printf("The Value of a is %d \n", a);
     /*or*/
-    printf ("The Value of b is %f \n" ,b );
+    printf("The Value of b is %f \n", b);
     /*or*/
-    printf ("The Value of c is %c \n" ,c );
-
+    printf("The Value of c is %c \n", c);
 
     return 0;
 }
