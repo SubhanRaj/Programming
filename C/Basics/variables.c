cd528ea2c6c901ac8ce78d7106455e7a81e8f784
Subhan Raj <shubhanraj2002@gmail.com>
2020-11-07 10:13:39 +0530

    Added Real, Integer & Character Variables

--- C/Basics/variables.c
+++ C/Basics/variables.c
@@ -4,8 +4,14 @@ This is my first c
 programm, which is awesome
 */
 
-int main()
-{
-    int a 
+int main() {
+
+    int a = 4;
+    // int b=7.5; //Not recommended beacuse 7.5 is not an integer, its a real variables
+
+    float a = 7.5;
+
+    char a = 'B';
+    
     return 0;
 }
