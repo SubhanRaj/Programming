ce02bd20ce6bbf4f6cf511e4de62ce1f846791e4
Subhan Raj <shubhanraj2002@gmail.com>
2020-11-09 09:27:52 +0530

    Added input file

--- C/Basics/Chapter 1/4input.c
+++ C/Basics/Chapter 1/4input.c
@@ -3,6 +3,15 @@
 
 int main()
  {
-     
+     int a, b;
+     printf("Enter the value of a\n");
+     scanf("%d",&a);
+
+     printf ("Enter the value of b\n");
+     scanf("%d",&b);
+
+     printf("The sum of a and b is %d", a + b);
+     printf("\n");
+
     return 0;
 }
