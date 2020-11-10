6a7c51066196b602408e94dc4a961c4d06910c0a
Subhan Raj <shubhanraj2002@outlook.com>
2020-11-10 17:07:25 +0530

    Written a programm for Calculating are of Circle from the predefined radius, providing Value of Pie = 3.14

--- C/Basics/Chapter 1/02practise_a.c
+++ C/Basics/Chapter 1/02practise_a.c
@@ -1,6 +1,12 @@
 #include <stdio.h> 
+// This programm calculates the area of a circle, providing value of Pie = 3.14
 
 int main(){
+    
+    float radius = 12;
+    float pie = 3.14;
+
+    printf("The are of circle for given radius is- %f", pie * radius * radius);
     
     return 0;
 }
