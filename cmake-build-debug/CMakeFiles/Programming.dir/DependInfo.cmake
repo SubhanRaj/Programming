24d05ffda7dffa3116b21081afb98549d2d11ab4
Subhan Raj <shubhanraj2002@outlook.com>
2020-11-11 22:14:53 +0530

    Configured CLion

---
+++ cmake-build-debug/CMakeFiles/Programming.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Programming/C/Basics/Chapter 1/01practise_a.c" "C:/Programming/cmake-build-debug/CMakeFiles/Programming.dir/C/Basics/Chapter_1/01practise_a.c.obj"
+  "C:/Programming/C/Basics/Chapter 1/01practise_b.c" "C:/Programming/cmake-build-debug/CMakeFiles/Programming.dir/C/Basics/Chapter_1/01practise_b.c.obj"
+  "C:/Programming/C/Basics/Chapter 1/02practise_a.c" "C:/Programming/cmake-build-debug/CMakeFiles/Programming.dir/C/Basics/Chapter_1/02practise_a.c.obj"
+  "C:/Programming/C/Basics/Chapter 1/02practise_b.c" "C:/Programming/cmake-build-debug/CMakeFiles/Programming.dir/C/Basics/Chapter_1/02practise_b.c.obj"
+  "C:/Programming/C/Basics/Chapter 1/03practise_a.c" "C:/Programming/cmake-build-debug/CMakeFiles/Programming.dir/C/Basics/Chapter_1/03practise_a.c.obj"
+  "C:/Programming/C/Basics/Chapter 1/03practise_b.c" "C:/Programming/cmake-build-debug/CMakeFiles/Programming.dir/C/Basics/Chapter_1/03practise_b.c.obj"
+  "C:/Programming/C/Basics/Chapter 1/1first.c" "C:/Programming/cmake-build-debug/CMakeFiles/Programming.dir/C/Basics/Chapter_1/1first.c.obj"
+  "C:/Programming/C/Basics/Chapter 1/2variables.c" "C:/Programming/cmake-build-debug/CMakeFiles/Programming.dir/C/Basics/Chapter_1/2variables.c.obj"
+  "C:/Programming/C/Basics/Chapter 1/3basicarthematic.c" "C:/Programming/cmake-build-debug/CMakeFiles/Programming.dir/C/Basics/Chapter_1/3basicarthematic.c.obj"
+  "C:/Programming/C/Basics/Chapter 1/4input.c" "C:/Programming/cmake-build-debug/CMakeFiles/Programming.dir/C/Basics/Chapter_1/4input.c.obj"
+  "C:/Programming/C/Basics/Chapter 1/CBoilerPlate.c" "C:/Programming/cmake-build-debug/CMakeFiles/Programming.dir/C/Basics/Chapter_1/CBoilerPlate.c.obj"
+  "C:/Programming/C/Basics/Chapter 1/Userinputpractise.c" "C:/Programming/cmake-build-debug/CMakeFiles/Programming.dir/C/Basics/Chapter_1/Userinputpractise.c.obj"
+  "C:/Programming/C/Basics/Chapter 2/new.c" "C:/Programming/cmake-build-debug/CMakeFiles/Programming.dir/C/Basics/Chapter_2/new.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../.idea"
+  "../.vs"
+  "../C"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Programming.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Programming.dir/C/Basics/Chapter_1/01practise_a.c.obj"
+  "CMakeFiles/Programming.dir/C/Basics/Chapter_1/01practise_b.c.obj"
+  "CMakeFiles/Programming.dir/C/Basics/Chapter_1/02practise_a.c.obj"
+  "CMakeFiles/Programming.dir/C/Basics/Chapter_1/02practise_b.c.obj"
+  "CMakeFiles/Programming.dir/C/Basics/Chapter_1/03practise_a.c.obj"
+  "CMakeFiles/Programming.dir/C/Basics/Chapter_1/03practise_b.c.obj"
+  "CMakeFiles/Programming.dir/C/Basics/Chapter_1/1first.c.obj"
+  "CMakeFiles/Programming.dir/C/Basics/Chapter_1/2variables.c.obj"
+  "CMakeFiles/Programming.dir/C/Basics/Chapter_1/3basicarthematic.c.obj"
+  "CMakeFiles/Programming.dir/C/Basics/Chapter_1/4input.c.obj"
+  "CMakeFiles/Programming.dir/C/Basics/Chapter_1/CBoilerPlate.c.obj"
+  "CMakeFiles/Programming.dir/C/Basics/Chapter_1/Userinputpractise.c.obj"
+  "CMakeFiles/Programming.dir/C/Basics/Chapter_2/new.c.obj"
+  "Programming.exe"
+  "Programming.exe.manifest"
+  "Programming.pdb"
+  "libProgramming.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Programming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
