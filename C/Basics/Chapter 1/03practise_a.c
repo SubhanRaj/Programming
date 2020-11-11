29398863684b2c6256cf3c8aaa8251d83e0d08eb
Subhan Raj <shubhanraj2002@outlook.com>
2020-11-11 21:52:40 +0530

    Created a programm to conver celsius to Fahrenheit.

--- C/Basics/Chapter 1/03practise_a.c
+++ C/Basics/Chapter 1/03practise_a.c
@@ -1,6 +1,15 @@
-#include <stdio.h> 
+#include <stdio.h>
+// This programm converts Degree Celsius to Fahrenheit from pre-given values
+// Formula- (°C × 9/5) + 32 = °F
 
 int main(){
+    float celsius = 37;
+    float far = (celsius * 9 / 5) + 32;
+
+    printf("\n");
+    printf("The Temprature of Human Body in celsius is %f\n", celsius);
+    printf("And the same temprature in Fahrenheit will be %f\n", far);
     
+
     return 0;
 }
---
+++ C/Basics/Chapter 1/03practise_b.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// This programm converts Degree Celsius to Fahrenheit by taking input from the user.
+// Formula- (°C × 9/5) + 32 = °F
+
+int main(){
+    float celsius;
+    float far = (celsius * 9 / 5) + 32;
+
+    printf ("Enter the temprature in Degree Celsius\n");
+    scanf ("%f", &celsius);
+    printf ("\n");
+    printf("And the same temprature in Fahrenheit will be %f\n", (celsius * 9 / 5) + 32);
+
+    return 0;
+}
