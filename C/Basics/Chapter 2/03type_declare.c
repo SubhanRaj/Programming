d2968f7181654ec659ae9730d1327b27dc4b455a
Subhan Raj <shubhanraj2002@gmail.com>
2020-11-15 21:16:37 +0530

    Some practical.

---
+++ C/Basics/Chapter 2/03type_declare.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+// A simple programm for understanding Type of C Instructions
+
+int main()
+{
+    int a = 4;          // Type declaration instruction
+    int b , c;  // Type declaration instruction
+    c = b = a;    //Type declaration instruction
+
+    printf("\n");
+    printf("The value of numbers entered as Integer a is %d \n,", a);
+    printf("\n");
+    printf("The value of number entered as Integer b is %d\n", b);
+    printf("\n");
+    printf("The value of number entered as Integer c is %d\n", c);
+    printf("\n");
+    
+    
+
+    return 0;
+}
---
+++ C/Basics/Chapter 2/04type_declare.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+// A simple programm for understanding Type of C Instructions
+
+int main()
+{
+    float a = 8.9;          // Type declaration instruction
+    float b = 1.1; // Type declaration instruction
+    float c = a + b;     //Type declaration instruction
+
+    printf("\n");
+    printf("The value of numbers entered as variable a is %f \n,", a);
+    printf("\n");
+    printf("The value of number entered as variable b is %f\n", b);
+    printf("\n");
+    printf("The value of number entered as variable c is %f\n", c);
+    printf("\n");
+
+    return 0;
+}
