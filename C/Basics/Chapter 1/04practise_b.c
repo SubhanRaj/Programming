3641a854a53e58584ed6846293364d5bfc85b804
Subhan Raj <shubhanraj2002@gmail.com>
2020-11-16 10:40:16 +0530

    created a simple interest calculator in C Language.

---
+++ C/Basics/Chapter 1/04practise_b.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int p, t;
+    float r;
+
+    printf("\n");
+
+    printf("Enter the pricipal amount\n");
+    scanf("%d", &p);
+
+    printf("Enter the time period in year\n");
+    scanf("%d", &t);
+
+    printf("Enter the rate of interest \n");
+    scanf("%f", &r);
+
+    float si = p * t * r / 100;
+
+    printf("The simple interest will be %f\n", si);
+    printf("\n");
+    printf("and the final amount will be- %f\n", si + p);
+
+    printf("\n");
+
+    return 0;
+}
