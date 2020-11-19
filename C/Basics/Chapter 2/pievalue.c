e221ec708825eb1a523a228fdaafe421899dc3a7
Subhan Raj <shubhanraj2002@outlook.com>
2020-11-19 16:06:15 +0530

    Created a C prgramm to calculate value of Pi upto 800 digits.

--- C/Basics/Chapter 2/pievalue.c
+++ C/Basics/Chapter 2/pievalue.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-// This C programm Caluclates value of Pie upto 6 digits of decimal.
+#include <math.h>
+// This C programm Caluclates value of Pi upto 6 digits of decimal.
 int main()
 {
     double a = 22;
     double b = 7;
-    double pie = a / b;
+    double pi = a / b;
     
     printf("\n");
-    printf("The value of Pie is %f\n", pie);
+    printf("The value of Pi is %f\n", pi);
     printf("\n");
 
     return 0;
