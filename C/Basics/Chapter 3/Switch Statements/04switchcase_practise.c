9bf5f46d6c248169002b4ab9442b1a2b28c13d91
Subhan Raj <shubhanraj2002@gmail.com>
2020-11-25 12:14:29 +0530

    Started working on a new C program.

---
+++ C/Basics/Chapter 3/Switch Statements/04switchcase_practise.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+// This C program is a choise guess
+
+int main()
+{
+
+    char fruit;
+    char vegetables;
+    char fastfood;
+    
+    printf("Welcome dear \n");
+    printf("Did you eat fruits?\n");
+    printf("Enter 'Y' for Yes and 'N' for No \n");
+    scanf("%c", &fruit);
+
+    printf("Did you eat fruits?\n");
+    printf("Enter 'Y' for Yes and 'N' for No \n");
+    scanf("%c", &fruit);
+
+    printf("Did you eat fruits?\n");
+    printf("Enter 'Y' for Yes and 'N' for No \n");
+    scanf("%c", &fruit);
+
+    // int fruit1, fruit2, fruit3, fruit4, fruit5;
+    // int vegetable1, vegetable2, vegetable3, vegetable4, vegetable5;
+
+    // printf("\n");
+    // printf("Welcome dear \n");
+    // printf("let's play a game\n");
+    // printf("Enter name of any five fruits- \n");
+    // scanf("%d, %d, %d, %d, %d, %d", &fruit1, &fruit2, &fruit3, &fruit4, &fruit5);
+
+    // printf("Pretty great \n");
+
+    // printf("Now enter name of 5 vegetable- \n");
+    // scanf("%d, %d, %d, %d, %d", &vegetable1, &vegetable2, &vegetable3, &vegetable4, &vegetable5);
+    // printf("Thanks a lot \n");
+    
+    // if (fruit1 == )
+    // {
+    //     /* code */
+    // }
+    
+
+    return 0;
+    
+}
