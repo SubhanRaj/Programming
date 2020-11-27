32070ebd1c2945a1f65c1a9e58cb14a81777c812
Subhan Raj <shubhanraj2002@outlook.com>
2020-11-27 21:04:12 +0530

    New code.

--- C/Basics/Chapter 3/Switch Statements/04switchcase_practise.c
+++ C/Basics/Chapter 3/Switch Statements/04switchcase_practise.c
@@ -8,7 +8,7 @@ int main()
     char fruit;
     char vegetables;
     char fastfood;
-    
+
     printf("Welcome dear \n");
     printf("Did you eat fruits?\n");
     printf("Enter 'Y' for Yes and 'N' for No \n");
@@ -18,9 +18,9 @@ int main()
     printf("Enter 'Y' for Yes and 'N' for No \n");
     scanf("%c", &fruit);
 
-    printf("Did you eat fruits?\n");
+    printf("Did you eat Vegetables?\n");
     printf("Enter 'Y' for Yes and 'N' for No \n");
-    scanf("%c", &fruit);
+    scanf("%c", &vegetables);
 
     // int fruit1, fruit2, fruit3, fruit4, fruit5;
     // int vegetable1, vegetable2, vegetable3, vegetable4, vegetable5;
@@ -36,13 +36,11 @@ int main()
     // printf("Now enter name of 5 vegetable- \n");
     // scanf("%d, %d, %d, %d, %d", &vegetable1, &vegetable2, &vegetable3, &vegetable4, &vegetable5);
     // printf("Thanks a lot \n");
-    
+
     // if (fruit1 == )
     // {
     //     /* code */
     // }
-    
 
     return 0;
-    
 }
