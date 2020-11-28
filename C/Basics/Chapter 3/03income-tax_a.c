aa3218c50f2818e8c63a68ecde1a4d5ad1f1647c
Subhan Raj <shubhanraj2002@gmail.com>
2020-11-28 21:54:07 +0530

    Created a C Programm to calculate income tax from user given input.

--- C/Basics/Chapter 3/03income-tax_a.c
+++ C/Basics/Chapter 3/03income-tax_a.c
@@ -1,29 +1,27 @@
-#include <stdio.h>
+#include <stdio.h> 
 
-int main()
-{
-
-    float income;
-    float tax;
+int main(){
     
+    float tax = 0, income;
+
     printf("\n");
     printf("Enter your income\n");
     scanf("%f", &income);
 
-    if (income >= 250000 && income <= 500000)
+    if ( income >= 250000 && income <= 500000)
     {
         tax = tax + 0.05 * (income - 250000);
     }
-    if (income >= 500000 && income < 1000000)
+    if (income >= 500000 && income <= 1000000)
     {
         tax = tax + 0.20 * (income - 500000);
     }
-    if (income >= 100000)
+    if (income >= 1000000)
     {
-        tax = tax + 0.30 * (income - 1000000);
+        tax = tax + 0.30 * (income - 500000);
     }
-    printf("Your income is %f .\n", income);
-    printf("The tax you have to pay is %f \n", tax);
+
+    printf("Your net income tax to be apid is %f \n", tax);
 
     return 0;
 }
