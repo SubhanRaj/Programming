4de78562decea54f4889db639f44feaf10bcf18e
Subhan Raj <shubhanraj2002@gmail.com>
2020-12-27 20:06:07 +0530

    Created Directory 'Practise' inside 'C' for C Language Practise.

---
+++ C/C Practise Programms/Multiplication_Table.c
@@ -0,0 +1,33 @@
+// A C prpogram to print multiplication table of a given number n upto range n
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+
+    int i;
+    int n;
+    int r;
+
+    printf("\n");
+
+    printf("This c programm prints multiplication table of any given number 'n' for range r \n");
+    printf("Enter the value of n- ");
+    scanf("%d", &n);
+
+    printf("Enter the range of r- ");
+    scanf("%d", &r);
+
+    printf("\n");
+
+    printf("**** Multiplication table of %d  upto range %d **** \n \n", n, r);
+
+    for (i = 1; i <= r; i++)
+    {
+        printf("%d X %d = %d \n", n, i, n * i);
+    }
+    printf("\n");
+
+    return 0;
+}
---
+++ C/C Practise Programms/readme.md
@@ -0,0 +1,5 @@
+# C Programs written for Practise  
+
+- Practiseing and making my Knowledge of C language better by wrinti programs, The directry [Basic](https://github.com/SubhanRaj/Programming/tree/main/C/Basics) contains main example programms according to the chapter.
+
+- In this folder there are some crazy C programs that can even hang up your PC. Be careful !!!
