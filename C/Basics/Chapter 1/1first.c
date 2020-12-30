82b3a14c91f1570e281ebf940f81672cdcd788f7
Subhan Raj <shubhanraj2002@gmail.com>
2020-12-30 09:46:41 +0530

    Few changes.

--- C/Basics/Chapter 1/1first.c
+++ C/Basics/Chapter 1/1first.c
@@ -1,16 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 /*
 This is my first c
 programm, which is awesome
 */
 
-int main(){
+int main()
+{
     int tom;
     // This is a single line comment
     /* This is a multi line 
     comment*/
-
-    printf("Hello, World! I'm Subhan Raj, and started learnig C language \n");
+    printf("\n");
+    printf("Hello, World!\n");
+    printf("I'm Subhan Raj, and started learnig C language \n");
     printf("today as on 19-Oct-2020 \n");
+    printf("\n");
     return 0;
-} 
+}
---
+++ C/C Practise Programs/Authentication.c
@@ -0,0 +1,32 @@
+// A Complex authentication program in C Language
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int name;     // name
+    int age;      // Age of user
+    int income;   //Annual income of user
+    int spouse;   //Maritial status
+    int child;    // parenting status of user
+    int dlapply;  //Appling for driving licence
+    int dlown;    //have driving licence
+    int passport; //passport of user
+    int cone;
+    int ctwo;
+    int cthree;
+    int cfour;
+    int cfive;
+    int csix;
+    int cseven; // choices
+
+    printf("\n");
+    printf("Hi dear, what's your name?");
+    scanf("%d", &name);
+
+    printf("Welcome to Regulation Authority %d \n \n", name);
+
+    return 0;
+}
