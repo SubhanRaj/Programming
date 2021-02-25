fb4fa9bd9f06d1479f21eecf9c1fe5275cdd6c10
subhanraj <shubhanraj2002@gmail.com>
2021-02-25 20:53:26 +0530

    A C program that calculates the Simple Interest. The Principal Amount, Rate of interest and Time are entered through the keyboard.

---
+++ C/BTech 1st Sem Lab Practise (CS 151)/02.c
@@ -0,0 +1,42 @@
+// Question: Write a program that calculates the Simple Interest and Compound Interest. The Principal Amount, Rate of interest and Time are entered through the keyboard.
+
+// Objective: To create a C program that calculates the Simple Interest and Compound Interest. The Principal Amount, Rate of interest and Time are entered through the keyboard.
+
+/*---------------Code---------------*/
+// A C program that calculates the Simple Interest and Compound Interest. The Principal Amount, Rate of interest and Time are entered through the keyboard.
+// Author: Subhan Raj
+#include <stdio.h>
+
+int main()
+{
+    int pa, roi, tp; // pa= Principal Amount: roi= rate of intrest : tp = time period
+    int si;          // si= simple intrest
+    int fa;          // Final Amount
+    printf("\n");
+
+    printf("This C Program calculates Simple and Compound Intrest from the user inputted values.");
+    /* Input principle, time and rate */
+    printf("\nEnter Principal Amount- ");
+    scanf("%d", &pa);
+    printf("\nEnter Rate of Intrest- ");
+    scanf("%d", &roi);
+    printf("\nEnter Time period in months- ");
+    scanf("%d", &tp);
+    /* Calculation of Simple Intrets */
+    si = (pa * roi * tp) / 100;
+    fa = si + pa;
+
+    printf("\nThe simple intreast on Rs %d at an Intrest rate of %d after %d months will be %d and the total amount will be %d\n", pa, roi, tp, si, fa);
+    return 0;
+}
+
+// Output of the Above program-
+
+/*This C Program calculates Simple and Compound Intrest from the user inputed values.
+Enter Principal Amount- 100
+
+Enter Rate of Intrest- 10
+
+Enter Time period in months- 12
+
+The simple intrest on Rs 100 at Intrest rate of 10 after 12 months will be 120 and the total amount will be 220 */
