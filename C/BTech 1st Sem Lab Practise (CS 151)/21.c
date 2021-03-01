f8fa7b805b613e5875ead2ac1c1220f83c68184d
subhanraj <shubhanraj2002@gmail.com>
2021-03-01 15:07:07 +0530

    19: C program to convert the binary number into a decimal number and vice versa. 21 : C program that inputs two arrays and saves the sum of corresponding elements of these arrays in a third array and prints them.

---
+++ C/BTech 1st Sem Lab Practise (CS 151)/21.c
@@ -0,0 +1,39 @@
+/* Question 19. WAP that inputs two arrays and saves the sum of corresponding elements of these
+arrays in a third array and prints them.*/
+
+/* Objective : To create a C program that inputs two arrays and saves the sum of corresponding elements of these
+arrays in a third array and prints them.*/
+
+/*---------------Code---------------*/
+#include <stdio.h> 
+
+int main(){
+    int a [2], b[2], c[2], i;
+    for (i = 0; i <2; i++)
+    {
+        printf("\nEnter the elements of array a [i] ");
+        scanf("%d", &a[i]);
+    }
+    for (i = 0; i < 2; i++)
+    {
+        printf("\nEnter the elements of array b [i] ");
+        scanf("%d", &b[i]);
+    }
+    for (i = 0; i < 2; i++)
+    {
+        c [i]= a[i] + b[i];
+    }
+    for (i = 0; i < 2; i++)
+    {
+        printf("%d" ,c[i]);
+    }
+    
+    
+    
+    return 0;
+}
+
+// Program to convert decimal to binary
+// Author : Subhan Raj
+
+// Output of the above program
