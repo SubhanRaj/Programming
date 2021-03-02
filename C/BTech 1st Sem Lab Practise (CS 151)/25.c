c1dece153a2a4bc7cc3882bcadf537753d45f1ff
subhanraj <shubhanraj2002@gmail.com>
2021-03-02 09:24:09 +0530

    C program that finds the sum of diagonal elements of an MxN matrix

---
+++ C/BTech 1st Sem Lab Practise (CS 151)/25.c
@@ -0,0 +1,48 @@
+// Question24. WAP that finds the sum of diagonal elements of an MxN matrix
+/* Objective :To create a c program that finds the sum of diagonal elements of an MxN matrix*/
+
+/*---------------Code---------------*/
+
+//C program that finds the sum of diagonal elements of an MxN matrix
+//Author: Subhan Raj
+
+#include <stdio.h>
+
+void main()
+{
+    int mat[12][12];
+    int i, j, row, col, sum = 0;
+    printf("Enter the number of rows and columns for 1st matrix\n");
+    scanf("%d%d", &row, &col);
+    printf("Enter the elements of the matrix\n");
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    printf("The matrix\n");
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            printf("%d\t", mat[i][j]);
+        }
+        printf("\n");
+    }
+    //To add diagonal elements
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            if (i == j)
+            {
+                sum = sum + mat[i][j];
+            }
+        }
+    }
+
+    printf("The sum of diagonal elements of a square matrix = %d\n", sum);
+}
