a4240c737eb3c96dbb9d4760c13d2a634dd303d3
subhanraj <shubhanraj2002@gmail.com>
2021-03-02 07:11:30 +0530

    20.c: A C program that simply takes elements of the array from the user and finds the sum of these elements. 21.c: A C program that inputs two arrays and saves the sum of corresponding elements of these arrays in a third array and prints them.

---
+++ C/BTech 1st Sem Lab Practise (CS 151)/20.c
@@ -0,0 +1,28 @@
+/* Question 20. WAP that simply takes elements of the array from the user and finds the sum of these
+elements.*/
+
+/* Objective : To create a C program that simply takes elements of the array from the user and finds the sum of these
+elements.*/
+
+/*---------------Code---------------*/
+// Author: Subhan Raj
+#include <stdio.h>
+
+int main()
+{
+    int a[1000], i, n, sum = 0;
+    printf("\nEnter size of the array: ");
+    scanf("%d", &n);
+    printf("\nEnter elements in array ");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for (i = 0; i < n; i++)
+    {
+        sum += a[i];
+        printf("\nSum of array is: %d\n", sum);
+    }
+
+    return 0;
+}
--- C/BTech 1st Sem Lab Practise (CS 151)/21.c
+++ C/BTech 1st Sem Lab Practise (CS 151)/21.c
@@ -1,15 +1,16 @@
-/* Question 19. WAP that inputs two arrays and saves the sum of corresponding elements of these
+/* Question 21. WAP that inputs two arrays and saves the sum of corresponding elements of these
 arrays in a third array and prints them.*/
 
 /* Objective : To create a C program that inputs two arrays and saves the sum of corresponding elements of these
 arrays in a third array and prints them.*/
 
 /*---------------Code---------------*/
-#include <stdio.h> 
+#include <stdio.h>
 
-int main(){
-    int a [2], b[2], c[2], i;
-    for (i = 0; i <2; i++)
+int main()
+{
+    int a[2], b[2], c[2], i;
+    for (i = 0; i < 2; i++)
     {
         printf("\nEnter the elements of array a [i] ");
         scanf("%d", &a[i]);
@@ -21,19 +22,12 @@ int main(){
     }
     for (i = 0; i < 2; i++)
     {
-        c [i]= a[i] + b[i];
+        c[i] = a[i] + b[i];
     }
     for (i = 0; i < 2; i++)
     {
-        printf("%d" ,c[i]);
+        printf("%d", c[i]);
     }
-    
-    
-    
+printf("\n");
     return 0;
 }
-
-// Program to convert decimal to binary
-// Author : Subhan Raj
-
-// Output of the above program
