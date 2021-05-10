11cdcafcf2db646557e3ae3da4093cbc784af218
SubhanRaj <shubhanraj2002@gmail.com>
2021-05-10 15:36:47 +0530

    Basic Dart Programs

---
+++ Dart/Basics/sample.dart
@@ -0,0 +1,5 @@
+// This is a sample dart program
+
+void main() {
+  print("Hi, This is Subhan Raj, I'm coding in Dart");
+}
---
+++ Dart/Basics/variable.dart
@@ -0,0 +1,9 @@
+// Variables in Dart
+
+void main() {
+  var firstName =
+      'Subhan '; //var can store any type of data like a numeber, special character or alphabet/s
+  String lastName = 'Raj'; //Sting can only store a character string
+
+  print(firstName + '' + lastName);
+}
