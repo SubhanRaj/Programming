7a983314e696935cc8961b1600ca6278219948f8
SubhanRaj <shubhanraj2002@gmail.com>
2021-06-03 11:01:06 +0530

    Basic JS console commands

---
+++ JavaScript/Basics/index.js
@@ -0,0 +1,17 @@
+alert("Welcome to JavaScript Practise");
+
+console.log ("Hello World");
+
+console.log (true)
+console.log (34);
+console.log ([10,20,30,40]) //Array in console
+console.log ({Subhan: 'Test1', value:100})
+console.table({Subhan: 'Test1', value:100})
+
+// Warning
+
+console.warn('This is a warning')
+
+// Clear Console
+
+console.clear()
