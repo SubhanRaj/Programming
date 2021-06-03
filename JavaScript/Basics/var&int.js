51d5b5d90a0a252dea188a4298a73b31b3142c21
SubhanRaj <shubhanraj2002@gmail.com>
2021-06-03 22:09:34 +0530

    CSS Practise

---
+++ JavaScript/Basics/var&int.js
@@ -0,0 +1,22 @@
+// variablles in JavaScript
+console.log('Variables');
+
+/* There are following ways for declaring Variable
+
+:---> var
+:---> let
+:---> const
+
+*/
+
+var title = "JavaScript Practise";
+
+var subtitle;
+subtitle = 'With Code With Harry in'
+
+var duration = 15;
+var timeperiod = "Days";
+
+console.log(title, subtitle,duration,timeperiod) ;
+
+
