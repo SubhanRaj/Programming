1dd998cea80ba3dc3edb765e4755c3164e51bb8f
SubhanRaj <shubhanraj2002@gmail.com>
2021-06-25 08:24:14 +0530

    Loaded Data from JSON File Day 13 Completed

--- Flutter/flutter_catalogue/lib/pages/home_page.dart
+++ Flutter/flutter_catalogue/lib/pages/home_page.dart
@@ -2,9 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:flutter_catalogue/models/catalogue.dart';
 import 'package:flutter_catalogue/widgets/drawer.dart';
 import 'package:flutter_catalogue/widgets/item_widget.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    // ignore: unused_local_variable
+    final catalogueJson =
+        await rootBundle.loadString("assets/files/catalogue.json");
+    final decodedData = jsonDecode(catalogueJson);
+    var productsData = decodedData["products"];
+  }
 
   @override
   Widget build(BuildContext context) {
