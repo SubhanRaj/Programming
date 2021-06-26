c51b449dac0114f686a934a0a64e34496669034c
SubhanRaj <shubhanraj2002@gmail.com>
2021-06-26 20:44:41 +0530

    Completed Day 16 Swithced from List View to Grid View Updated Name of App in  Android Manifest

--- Flutter/flutter_catalogue/lib/pages/home_page.dart
+++ Flutter/flutter_catalogue/lib/pages/home_page.dart
@@ -42,12 +42,46 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
           padding: const EdgeInsets.all(16.0),
           child:
+              // ignore: unnecessary_null_comparison
               (CatalogueModel.items != null && CatalogueModel.items.isNotEmpty)
-                  ? ListView.builder(
-                      itemCount: CatalogueModel.items.length,
-                      itemBuilder: (context, index) => ItemWidget(
-                        item: CatalogueModel.items[index],
+                  ? GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        mainAxisSpacing: 20,
+                        crossAxisSpacing: 16,
                       ),
+                      itemBuilder: (context, index) {
+                        final item = CatalogueModel.items[index];
+                        return Card(
+                          clipBehavior: Clip.antiAlias,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          child: GridTile(
+                            header: Container(
+                              child: Text(
+                                item.name,
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              padding: const EdgeInsets.all(12),
+                              decoration: BoxDecoration(
+                                color: Colors.deepPurple,
+                              ),
+                            ),
+                            child: Image.network(item.image),
+                            footer: Container(
+                              child: Text(
+                                item.price.toString(),
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              padding: const EdgeInsets.all(12),
+                              decoration: BoxDecoration(
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                      itemCount: CatalogueModel.items.length,
                     )
                   : Center(
                       child: CircularProgressIndicator(),
