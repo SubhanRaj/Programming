29cf51a75beca8d55d33fa6e7c2f3c8974f22786
SubhanRaj <shubhanraj2002@gmail.com>
2021-08-01 11:26:04 +0530

    Hello World

--- C++/Basics/helloworld.cpp
+++ C++/Basics/helloworld.cpp
@@ -8,3 +8,9 @@ int main()
     return 0;
 }
 
+// C++ is an Object-Oriented Programming Language, it has classes and objects.
+
+// cout is a class, it is a stream of characters. Defiend in the standard library.
+//  endl is a function, it is a newline character. Defined in the standard library.
+// Use of return in a function is optional.
+// iostream is a namespace, it is a collection of classes and functions. Defined in the standard library.
