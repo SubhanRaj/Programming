b302c76e1c16b2f61e6460c106a8ad034b53a8a5
SubhanRaj <shubhanraj2002@gmail.com>
2021-08-01 11:25:47 +0530

    Input & Output

---
+++ C++/Basics/output.cpp
@@ -0,0 +1,14 @@
+// Output in C++
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello\nWorld" << endl
+         << "How are you?" << endl;
+    return 0;
+}
+
+// Multi line comment
+
+/* Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. */
