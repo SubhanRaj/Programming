8c58b94fc0ccb1dd555afab33bd301b0ba327479
SubhanRaj <shubhanraj2002@gmail.com>
2021-08-31 20:31:32 +0530

    variables and sums in C++

---
+++ C++/Basics/variables_and_comments.cpp
@@ -0,0 +1,23 @@
+// variables and Comments in C++
+
+#include <iostream>
+
+using namespace std; // Don't forget to put ';' here
+int main()
+{
+    // This is how we do a single line comment in C++
+    /* And here
+    Comes 
+    a
+    multi-line
+    comment in C++
+    */
+
+    //   Creating Variables
+
+    int a = 6;
+    int b = 12;
+    int sum = a + b;
+
+    cout << "Sum of a & b = " << sum;
+}
