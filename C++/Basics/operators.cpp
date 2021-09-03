b746a12ae64c56501b055393f3b37f45f20e780e
SubhanRaj <shubhanraj2002@gmail.com>
2021-09-03 07:58:55 +0530

    Operators in C++

---
+++ C++/Basics/operators.cpp
@@ -0,0 +1,38 @@
+// Arthematic Operators in C++
+
+#include <iostream>
+using namespace std;
+
+void arth()
+{
+    int i, j;
+    // Arthematic Operators in C++
+    cout << "Enter int i: ";
+    cin >> i;
+    cout << "Enter int j: ";
+    cin >> j;
+    cout << ("i+j= \n") << i + j << endl; // Addition
+    cout << ("i-j= \n") << i - j << endl; // Substraction
+    cout << ("i*j= \n") << i * j << endl; // Multiplication
+    cout << ("i/j= \n") << i / j << endl; // Division
+    cout << ("i%j= \n") << i % j << endl; // Modulus Division
+}
+
+void inc_dec()
+{
+    // Increment & decrement Operators
+    int i;
+    // Increment & Decrement Operators in C++
+    cout << "Enter int i: ";
+    cin >> i;
+    cout << ("The Value of i++ is: ") << i++ << endl;
+    cout << ("The Value of ++i is: ") << ++i << endl;
+    cout << ("The Value of i-- is: ") << i-- << endl;
+    cout << ("The Value of --i is: ") << --i << endl;
+}
+
+int main()
+{
+    arth();
+    inc_dec();
+}
