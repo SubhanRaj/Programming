6af232846419616c47f35f452f8c23f018286600
SubhanRaj <shubhanraj2002@gmail.com>
2021-09-09 10:56:30 +0530

    Added do-while to run the program till the user wants to exit

--- C++/OOPS/operator_overloading.cpp
+++ C++/OOPS/operator_overloading.cpp
@@ -23,28 +23,42 @@ public:
     {
         cout << "Box wiil have biscuts" << endl;
     }
+    void exit(int e)
+    {
+        cout << "Thank you for using our service" << endl;
+        exit(0);
+    }
+
 };            // end of class Kitchen
 void choice() // function to display menu
 {
     int ch;
     Kitchen Lunchbox; // object of class Kitchen
-    cout << "What you want for Lunch?" << endl;
-    cout << "Enter 1 for Breakfast,2 for lunch, 3 for Snacks" << endl;
-    cin >> ch;
-    switch (ch) // switch case to select choice
+
+    do // do-while to run the program till the user wants to exit
     {
-    case 1:
-        Lunchbox.breakFast(1);
-        break;
-    case 2:
-        Lunchbox.lunch(2);
-        break;
-    case 3:
-        Lunchbox.snaks(3);
-        break;
-    default:
-        cout << "Invalid Choice" << endl;
-    } // end of switch case
+        cout << "\nWhat you want for Lunch?" << endl;
+        cout << "Enter 1 for Breakfast,2 for lunch, 3 for Snacks, 4 for exit" << endl;
+        cin >> ch;
+        switch (ch) // switch case to select choice
+        {
+        case 1:
+            Lunchbox.breakFast(1);
+            break;
+        case 2:
+            Lunchbox.lunch(2);
+            break;
+        case 3:
+            Lunchbox.snaks(3);
+            break;
+        case 4:
+            cout << "exit";
+            break;
+
+        default:
+            cout << "Invalid Choice" << endl;
+        }
+    } while (ch != 4); // end of switch case
 }
 int main() // main function
 {
