c1f04b60124a77f34722eb601fc9b7baf43d022c
Subhan Raj <shubhanraj2002@gmail.com>
2021-10-07 07:58:06 +0530

    Added Comments

--- C++/Practise/addition_of_two_numbers_without_arthematic_operators.cpp
+++ C++/Practise/addition_of_two_numbers_without_arthematic_operators.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 int main()
 {
-    int num1, num2, i;
-    cout << "Enter the first number: " ;cin >> num1;
+    int num1, num2, i; // Declaring the variables
+    cout << "Enter the first number: " ;cin >> num1; // Taking the input from the user
     cout << "Enter the second number: " ;cin >> num2;
-    for (i = 0; i < num2; i++)
+    for (i = 0; i < num2; i++) // Loop to add the numbers
     {
-        num1++;
+        num1++; 
     }
     cout << "Sum of two numbers is: " << num1;
 }
