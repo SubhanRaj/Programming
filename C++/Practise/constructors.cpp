9d0b142030bfdc422698d3b257fbcb0aee261692
SubhanRaj <shubhanraj2002@gmail.com>
2021-10-12 05:42:33 +0530

    Constructors Practise

---
+++ C++/Practise/constructors.cpp
@@ -0,0 +1,26 @@
+// Constructors in C++
+#include <iostream>
+using namespace std;
+class A
+{
+public:
+    int x;
+
+public:
+    A()
+    {
+        x = 10;
+    }
+    void disp(int x)
+    {
+        cout << "The value of X is: " << x << endl;
+    }
+};
+int main()
+{
+    A obj;
+    cout << obj.x << endl;
+    obj.disp(20);
+
+    return 0;
+}
