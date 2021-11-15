ba2eb321ef44dd46d8f2a39c281b70a5e93b1f28
SubhanRaj <shubhanraj2002@gmail.com>
2021-11-15 22:13:07 +0530

    C++ Revision

---
+++ C++/CPP_Revision/hello_world.cpp
@@ -0,0 +1,8 @@
+using namespace std;
+#include <iostream> // include - Preprocessor directive to include the standard library
+// iostream is a namespace that contains the standard input and output streams
+
+int main() {
+  cout << "Hello World!" << endl;
+  return 0;
+}
