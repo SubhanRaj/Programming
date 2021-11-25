04026a952f259786050994bd7b1b8f296adba58d
Subhan raj <shubhanraj2002@gmail.com>
2021-11-26 04:45:50 +0530

    Dynamic memory Allocation in C

---
+++ C/DSA/Dynamic_memory_allocation/realloc.c
@@ -0,0 +1,31 @@
+// C program to demonstrate realloc()
+// It allocates memory for an array of integers and then reallocates it to a larger size.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int *ptr, i, n1, n2;
+    printf("Enter size: ");
+    scanf("%d", &n1);
+
+    ptr = (int *)malloc(n1 * sizeof(int));
+
+    printf("Addresses of previously allocated memory:\n");
+    for (i = 0; i < n1; ++i)
+        printf("%pc\n", ptr + i);
+
+    printf("\nEnter the new size: ");
+    scanf("%d", &n2);
+
+    // rellocating the memory
+    ptr = realloc(ptr, n2 * sizeof(int));
+
+    printf("Addresses of newly allocated memory:\n");
+    for (i = 0; i < n2; ++i)
+        printf("%pc\n", ptr + i);
+
+    free(ptr);
+
+    return 0;
+}
