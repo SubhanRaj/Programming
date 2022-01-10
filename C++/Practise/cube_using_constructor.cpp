74a472707bafdd00495f326729e4151231d3298b
SubhanRaj <shubhanraj2002@gmail.com>
2022-01-10 10:29:25 +0530

    C++ Program to find cube of any number using constructor which takes input from user

---
+++ C++/Practise/cube_using_constructor.cpp
@@ -0,0 +1,23 @@
+// C++ Program to find cube of any number using constructor which takes input from user
+#include <iostream>
+using namespace std;
+
+class Cube
+
+{
+    public: 
+    Cube()
+    {
+        int n;
+        cout<<"Enter a number: ";  
+        cin>>n;
+        cout<<"Cube of "<<n<<" is "<<n*n*n<<endl;
+
+    }
+};
+
+int main()
+{
+    Cube c;
+    return 0;
+}
