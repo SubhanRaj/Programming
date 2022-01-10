65a5751ee695771e80b992b761874042035f26c2
SubhanRaj <shubhanraj2002@gmail.com>
2022-01-10 10:38:10 +0530

    C++ Program to find cube of any number using constructor (default, parameterized & copy)

--- C++/Practise/cube_using_constructor.cpp
+++ C++/Practise/cube_using_constructor.cpp
@@ -1,4 +1,4 @@
-// C++ Program to find cube of any number using constructor which takes input from user
+// C++ Program to find cube of any number using constructor (default, parameterized & copy) which takes input from user
 #include <iostream>
 using namespace std;
 
@@ -13,10 +13,31 @@ public:
         cin >> n;
         cout << "Cube of " << n << " is " << n * n * n << endl;
     }
+    // Now we will create a parameterized constructor
+    Cube(int n)
+    {
+        cout << "Cube of " << n << " is " << n * n * n << endl;
+    }
+    // Now we will create a copy constructor
+    Cube(Cube &obj)
+    {
+        cout << "Cube of " << obj.n << " is " << obj.n * obj.n * obj.n << endl;
+    }
+    // Now we will create a destructor
+    ~Cube()
+    {
+        cout << "Destructor called" << endl;
+    }
 };
 
 int main()
 {
+    // Calling default constructor
     Cube c;
+    // Calling parameterized constructor
+    Cube c1(5);
+    // Calling copy constructor
+    Cube c2(c1);
+
     return 0;
 }
