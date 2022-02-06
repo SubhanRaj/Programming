e127291d0839e659402ea2ef287434136386b29d
SubhanRaj <shubhanraj2002@gmail.com>
2022-02-06 12:33:22 +0530

    C++ program to illustrate function overriding

---
+++ C++/Lab_Work/function_overriding.cpp
@@ -0,0 +1,44 @@
+// C++ program to illustrate function overriding
+
+/* If derived class defines same function as defined in its base class, it is known as function overriding in C++.
+It is used to achieve runtime polymorphism. It enables us to provide specific implementation of the
+function which is already provided by its base class. */
+
+#include <iostream>
+using namespace std;
+
+class BaseClass
+{
+public:
+    virtual void Display()
+    {
+        cout << "\nThis is Display() method"
+                " of BaseClass";
+    }
+    void Show()
+    {
+        cout << "\nThis is Show() method "
+                "of BaseClass";
+    }
+};
+
+class DerivedClass : public BaseClass
+{
+public:
+    // Overriding method - new working of
+    // base class's display method
+    void Display()
+    {
+        cout << "\nThis is Display() method"
+                " of DerivedClass";
+    }
+};
+
+// Driver code
+int main()
+{
+    DerivedClass dr;
+    BaseClass &bs = dr;
+    bs.Display();
+    dr.Show();
+}
