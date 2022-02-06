7cb1d011c248ee5069415ddd486a1e39288cbb85
SubhanRaj <shubhanraj2002@gmail.com>
2022-02-06 16:18:30 +0530

    C++ program to illustrate the concept of hierarchical inheritance

---
+++ C++/Lab_Work/hierarchical_inheritance.cpp
@@ -0,0 +1,41 @@
+// C++ program to illustrate the concept of hierarchical inheritance
+
+#include <iostream>
+using namespace std;
+
+class A // single base class
+{
+public:
+    int x, y;
+    void getdata()
+    {
+        cout << "\nEnter value of x and y:\n";
+        cin >> x >> y;
+    }
+};
+class B : public A // B is derived from class base
+{
+public:
+    void product()
+    {
+        cout << "\nProduct = " << x * y<<endl;
+    }
+};
+class C : public A // C is also derived from class base
+{
+public:
+    void sum()
+    {
+        cout << "\nSum = " << x + y <<endl;
+    }
+};
+int main()
+{
+    B obj1; // object of derived class B
+    C obj2; // object of derived class C
+    obj1.getdata();
+    obj1.product();
+    obj2.getdata();
+    obj2.sum();
+    return 0;
+}
